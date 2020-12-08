689fb41aaf8aaf8b7a62ef2c0721a28e1447d5cb
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2020-12-08 11:11:05 -0400

    implementado e estudando Join

---
+++ AluraTunes/AluraTunes/AluraTunes/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) {}
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Dblanche;Data Source=DESKTOP-777HC75\SQLSERVER2019");
+        }
+
+    }
+}
--- AluraTunes/AluraTunes/AluraTunes/Program.cs
+++ AluraTunes/AluraTunes/AluraTunes/Program.cs
@@ -1,23 +1,65 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Context;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 
+
 namespace AluraTunes
 {
-    public class contexto : DbContext
+  
+    static class  Program
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=alura;Data Source=DESKTOP-777HC75\SQLSERVER2019");
-        }
-    }
-    class Program
-    {
+        public static  AppDbContext _context = new AppDbContext();
         static void Main(string[] args)
         {
-            
+           
+
+             
+
+        }
+
+        public static void ConsultasComJoin()
+        {
+            //_context.Database.Log = Console.WriteLine();
+
+            var query = from c in _context.Categorias select c;
+
+            foreach (var categoria in query)
+            {
+                Console.WriteLine("{0}\t{1}", categoria.CategoriaId, categoria.Descricao);
+            }
+
+            var LanchesCategoria = from l in _context.Lanches
+                                   join c in _context.Categorias
+                                   on l.LancheId equals c.CategoriaId
+                                   select new { l, c };
+
+            Console.WriteLine();
+            foreach (var item in LanchesCategoria)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}", item.l.Nome, item.l.Preco,
+                                 item.c.CategoriaNome, item.c.Descricao);
+            }
+
+            Console.WriteLine();
+            var NLanchesCategoria = from l in _context.Lanches
+                                    join c in _context.Categorias
+                                    on l.CategoriaId equals c.CategoriaId
+                                    select new
+                                    {
+                                        CategoriaId = c.CategoriaId,
+                                        CategoriaNome = c.CategoriaNome,
+                                        LancheId = l.LancheId,
+                                        LancheNome = l.Nome
+                                    };
+
+            foreach (var item in NLanchesCategoria)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}", item.CategoriaId, item.CategoriaNome, item.LancheId, item.LancheNome);
+            }
+
         }
     }
 }
